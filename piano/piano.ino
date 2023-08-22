e6aa1318e7b52b255b510de422a451743ed35a9a
Nat-thapas <nathapas2548@gmail.com>
2023-08-22 21:32:59 +0700

    Finished piano, untested

--- piano/piano.ino
+++ piano/piano.ino
@@ -1,41 +1,98 @@
-#define NOTE_C4  262
-#define NOTE_D4  294
-#define NOTE_E4  330
-#define NOTE_F4  349
-#define NOTE_G4  392
-#define NOTE_A4  440
-#define NOTE_B4  494
+#include "pitches.h"
+#include "ssd.h"
 
 #define SPEAKER_PIN 5
-#define DEBUG_PIN 13
+#define MODE_PIN 13
+#define ACTION_PIN 4
+
+const int ssdPins[8] = {2, 3, A0, A1, A2, A3, A4, A5};
                        //C  D  E  F  G   A   B
 const int notePins[7] = {6, 7, 8, 9, 10, 11, 12};
-const int notes[7] = {NOTE_C4, NOTE_D4, NOTE_E4, NOTE_F4, NOTE_G4, NOTE_A4, NOTE_B4};
+const int notes[49] = {
+    NOTE_C1, NOTE_D1, NOTE_E1, NOTE_F1, NOTE_G1, NOTE_A1, NOTE_B1,
+    NOTE_C2, NOTE_D2, NOTE_E2, NOTE_F2, NOTE_G2, NOTE_A2, NOTE_B2,
+    NOTE_C3, NOTE_D3, NOTE_E3, NOTE_F3, NOTE_G3, NOTE_A3, NOTE_B3,
+    NOTE_C4, NOTE_D4, NOTE_E4, NOTE_F4, NOTE_G4, NOTE_A4, NOTE_B4,
+    NOTE_C5, NOTE_D5, NOTE_E5, NOTE_F5, NOTE_G5, NOTE_A5, NOTE_B5,
+    NOTE_C6, NOTE_D6, NOTE_E6, NOTE_F6, NOTE_G6, NOTE_A6, NOTE_B6,
+    NOTE_C7, NOTE_D7, NOTE_E7, NOTE_F7, NOTE_G7, NOTE_A7, NOTE_B7
+};
+
+SsdDriver ssd(ssdPins);
 
 void setup() {
     for (int i=0; i<7; i++) {
         pinMode(notePins[i], INPUT_PULLUP);
     }
-    pinMode(DEBUG_PIN, INPUT_PULLUP);
+    pinMode(MODE_PIN, INPUT_PULLUP);
+    pinMode(ACTION_PIN, INPUT_PULLUP);
     pinMode(SPEAKER_PIN, OUTPUT);
-    Serial.begin(9600);
+    // Serial.begin(9600);
 }
 
-unsigned recorded[256] = {};
+int recordedNotes[128] = {};
+long recordedLength[128] = {};
+
+int noteSpace = 3;
 
 int currentNote = 0;
 int lastNote = 0;
 
-int recordIdx = 0;
 bool recording = false;
+int recordIdx = 0;
 long recordMillis = 0;
 
+int modeIdx = 0;
+char modes[4] = "nrpc";
+char mode = 'n';
+
 void loop() {
+    // Drive ssd
+    switch (mode) {
+        case 'n':
+            ssd.setDisplay(23);
+            ssd.update();
+            break;
+        case 'r':
+            if (recording) {
+                ssd.setDisplay(40 + (millis()/500)%6);
+            } else {
+                ssd.setDisplay(27);
+            }
+            ssd.update();
+            break;
+        case 'p':
+            ssd.setDisplay(25);
+            ssd.update();
+            break;
+        case 'c':
+            ssd.setDisplay(12);
+            ssd.update();
+            break;
+        default:
+            mode = 'n';
+            ssd.setDisplay(23);
+            ssd.update();
+            break;
+    }
+
+    // Select mode
+    if (!digitalRead(MODE_PIN)) {
+        if (recording) {
+            recording = false;
+        }
+        modeIdx++;
+        modeIdx %= 4;
+        mode = modes[modeIdx];
+        delay(1000);
+    }
+
+    // Normal playing
     bool playing = false;
     for (int i=0; i<7; i++) {
         if (!digitalRead(notePins[i])) {
-            currentNote = i+1;
-            tone(SPEAKER_PIN, notes[i]);
+            currentNote = 7*noteSpace + i+1;
+            tone(SPEAKER_PIN, notes[7*noteSpace + i]);
             playing = true;
         }
     }
@@ -43,25 +100,62 @@ void loop() {
         currentNote = 0;
         noTone(SPEAKER_PIN);
     }
+
+    // Config
+    if (mode == 'c' && !digitalRead(ACTION_PIN)) {
+        noteSpace++;
+        noteSpace %= 7;
+        ssd.setDisplay(noteSpace);
+        ssd.update();
+        delay(1000);
+    }
+
+    // Recording
+    if (mode == 'r' && !digitalRead(ACTION_PIN)) {
+        if (recording) {
+            recording = false;
+        } else {
+            recording = true;
+            recordIdx = 0;
+            for (int i=0; i<128; i++) {
+                recordedNotes[i] = 0;
+                recordedLength[i] = 0;
+            }
+        }
+        delay(1000);
+    }
     if (currentNote != lastNote && recording) {
+        // Serial.println("Different detected!");
+        // Serial.println(recordIdx);
         int elapsed = millis() - recordMillis;
-        if (recordIdx != 0 || recorded[0] & (0b111 < 13) != 0) {
-            recorded[recordIdx] |= elapsed & 0b1111111111111;
+        if (recordIdx != 0 || recordedNotes[0] != 0) {
+            recordedLength[recordIdx] = elapsed;
             recordIdx++;
         }
         recordMillis = millis();
-        recorded[recordIdx] = currentNote << 13;
+        recordedNotes[recordIdx] = currentNote;
     }
     lastNote = currentNote;
-    if (!digitalRead(DEBUG_PIN)) {
-        for (int i=0; recorded[i]; i++) {
-            Serial.print(recorded[i] >> 13);
-            Serial.print(" ");
-            Serial.println(recorded[i] & 0b1111111111111);
-        }
-    }
-}
 
+    // Playback
+    if (mode == 'p' && !digitalRead(ACTION_PIN)) {
+        // Serial.println("Begining playback");
+        for (int i=0; i<recordIdx; i++) {
+            if (recordedNotes[i] > 0) {
+                // Serial.print("Playing: ");
+                // Serial.println(notes[recordedNote[i]-1]);
+                tone(SPEAKER_PIN, notes[recordedNotes[i]-1]);
+            } else {
+                noTone(SPEAKER_PIN);
+            }
+            // Serial.print("Delaying: ");
+            // Serial.println(recordedTime[i]);
+            delay(recordedLength[i]);
+        }
+        noTone(SPEAKER_PIN);
+    }
+    delay(50);
+}
 
 
 /*
@@ -89,8 +183,8 @@ void setup() {
     Serial.begin(9600);
 }
 
-int recordedNote[256] = {};
-long recordedTime[256] = {};
+int recordedNote[128] = {};
+long recordedTime[128] = {};
 
 int currentNote = 0;
 int lastNote = 0;
---
+++ piano/ssd.h
@@ -0,0 +1,88 @@
+class SsdDriver {
+    const int ssdMap[46] = {
+        // GFEDCBA  Segments      7-segment map:
+        0b0111111, // 0   "0"          AAA
+        0b0000110, // 1   "1"         F   B
+        0b1011011, // 2   "2"         F   B
+        0b1001111, // 3   "3"          GGG
+        0b1100110, // 4   "4"         E   C
+        0b1101101, // 5   "5"         E   C
+        0b1111101, // 6   "6"          DDD
+        0b0000111, // 7   "7"
+        0b1111111, // 8   "8"
+        0b1101111, // 9   "9"
+        0b1110111, // 65  'A'
+        0b1111100, // 66  'b'
+        0b0111001, // 67  'C'
+        0b1011110, // 68  'd'
+        0b1111001, // 69  'E'
+        0b1110001, // 70  'F'
+        0b0111101, // 71  'G'
+        0b1110110, // 72  'H'
+        0b0110000, // 73  'I'
+        0b0001110, // 74  'J'
+        0b1110110, // 75  'K'  Same as 'H'
+        0b0111000, // 76  'L'
+        0b0010101, // 77  'M'  Alternate display
+        0b1010100, // 78  'n'
+        0b0111111, // 79  'O'
+        0b1110011, // 80  'P'
+        0b1100111, // 81  'q'
+        0b1010000, // 82  'r'
+        0b1101101, // 83  'S'
+        0b1111000, // 84  't'
+        0b0111110, // 85  'U'
+        0b0111110, // 86  'V'  Same as 'U'
+        0b0101010, // 87  'W'  Alternate display
+        0b1110110, // 88  'X'  Same as 'H'
+        0b1101110, // 89  'y'
+        0b1011011, // 90  'Z'  Same as '2'
+        0b0000000, // 32  ' '  BLANK
+        0b1000000, // 45  '-'  DASH
+        0b1100011, // 42 '*'  DEGREE ..
+        0b0001000, // 95 '_'  UNDERSCORE
+        0b0000001, // LOADING: 1
+        0b0000010, // LOADING: 2
+        0b0000100, // LOADING: 3
+        0b0001000, // LOADING: 4
+        0b0010000, // LOADING: 5
+        0b0100000, // LOADING: 6
+    };
+    int ssdPins[8];
+    int displaying;
+    bool dpState;
+
+   public:
+    SsdDriver(int pins[]) {
+        for (int i=0; i<8; i++) {
+            this->ssdPins[i] = pins[i];
+            pinMode(pins[i], OUTPUT);
+        }
+        this->displaying = -1;
+        this->dpState = HIGH;
+        update();
+    }
+
+    void update() {
+        if (displaying < 0 || displaying > 45) {
+            for (int i=0; i<7; i++) {
+                digitalWrite(ssdPins[i], HIGH);
+            }
+            digitalWrite(ssdPins[7], !dpState);
+            return;
+        }
+        int ssdVal = this->ssdMap[displaying];
+        for (int i=0; i<7; i++) {
+            digitalWrite(ssdPins[i], ((ssdVal >> i) & 0b1) ^ 0b1);
+        }
+        digitalWrite(ssdPins[7], !dpState);
+    }
+
+    void setDisplay(int num) {
+        this->displaying = num;
+    }
+
+    void setDpState(bool state) {
+        this->dpState = state;
+    }
+};
